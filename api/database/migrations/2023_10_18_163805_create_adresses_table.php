b181ea279cc5f3c38d739c2e61bb8fe3623a8c66
Vinicius Vasconcelos Ferreira <viniciusvasconcelos.ferreira@gmail.com>
2023-10-18 14:44:44 -0300

    feat: inicializa a criação da seção de endereços
    BREAKING CHANGE: inserção de novos campos a model de usuários, criação dos recursos do módulo de endereços e ativação do soft delete para as seções

---
+++ api/database/migrations/2023_10_18_163805_create_adresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('adresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('postal_code');
+            $table->string('address');
+            $table->string('street')->nullable();
+            $table->string('number');
+            $table->string('neighborhood')->nullable();
+            $table->string('complement')->nullable();
+            $table->string('reference')->nullable();
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->boolean('set_default')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('adresses');
+    }
+};
